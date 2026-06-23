ade2d2747e98a1224e7f1c4fca6d80dc51ef6b27
Maksim <154775942+MIrrox27@users.noreply.github.com>
2026-06-23 19:30:46 +0500

    сделана новая версия слушателя http с функцией ввода своего адреса

--- src/main.rs
+++ src/main.rs
@@ -3,26 +3,41 @@
 
 //use axum::{Router, routing::get, extract::Path};
 //use tower_http::services::ServeDir;
-use tokio::{net::TcpListener, net::TcpStream, io::AsyncBufRead, spawn};
+use tokio::{io::{ AsyncReadExt, AsyncWriteExt}, net::{TcpListener}};
 use std::io;
 
 
 
 
 #[tokio::main]
-async fn main() {
-    println!("Please enter proxy addres [localhost:8000]"); // Просим ввести адресс для прокси
-    let mut addr = String::new();
-    io::stdin()
-        .read_line(&mut addr)
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    println!(" -- Please enter proxy addres [127.0.0.1:8000]>"); // Просим ввести адрес для прокси
+    let mut addres = String::new();
+    io::stdin().read_line(&mut addres)
         .expect("Error, can't read your addres");
 
-    if addr.is_empty(){addr = "localhost:8000".to_string()}
+    let addr = addres.trim();    
+    let addres = if addr.is_empty() {"127.0.0.1:8000".to_string()} else {addr.to_string()};
 
-    let listner = TcpListener::bind(addr);
+    println!("Addres:{}", addres);
+
+    let listner = TcpListener::bind(addres).await.unwrap();
+
+
     loop {
+        let (mut socket, addr) =  listner.accept().await.unwrap();
+        println!("New connect {}", addr);
         
+        tokio::spawn( async move  {
+            let mut buf = vec![0; 1024];
 
-    }
-    
+            loop {
+                match socket.read(&mut buf).await {
+                    Ok(0) => return, 
+                    Ok(n) => {if socket.write_all(&buf[..n]).await.is_err(){return ;}}
+                    Err(_) => return,
+                }
+            }
+        });
+    } 
 }
