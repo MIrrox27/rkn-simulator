182285606b20138d34a83cebf131c6f7fcc5c84c
Maksim <154775942+MIrrox27@users.noreply.github.com>
2026-06-23 09:18:50 +0500

    сделана функция для удаления домена из черного списка

--- c_src/filter.c
+++ c_src/filter.c
@@ -26,22 +26,34 @@ typedef struct DomensBlackList { // Своеобразный связанный 
   } DomensBlackList;
 
 
-  DomensBlackList *global_list = NULL; // Указатель на первый элемент массива 
+DomensBlackList *global_list = NULL; // Указатель на первый элемент массива 
 
 
 
-  void append_to_blacklist(const char *text){
-    DomensBlackList *new_node = (DomensBlackList*) malloc(sizeof(DomensBlackList));
-    if (new_node == NULL) return; 
+void append_to_blacklist(const char *text){
+  DomensBlackList *new_node = (DomensBlackList*) malloc(sizeof(DomensBlackList));
+  if (new_node == NULL) return; 
 
-    strncpy(new_node->str, text, sizeof(new_node->str) - 1);
-    new_node->str[sizeof(new_node->str) - 1] = '\0'; // Безопасное завершение строки
+  strncpy(new_node->str, text, sizeof(new_node->str) - 1); // Копируем строку с ограничением длины
+  new_node->str[sizeof(new_node->str) - 1] = '\0'; // Безопасное завершение строки
 
-    new_node->next = global_list;
-    global_list = new_node;
+  new_node->next = global_list;
+  global_list = new_node;
+}
 
 
-  }
+void delete_from_blacklist(const char *text){
+  DomensBlackList *current = global_list;
+
+  while (current != NULL){
+    DomensBlackList *temp = current;
+
+    if (strcmp(current->str, text)){
+      free(temp);
+    } 
+    current = current->next;
+  } 
+}
 
 
 
