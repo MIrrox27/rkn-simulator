b995e1981eb6a3c24196a42fcbd2a1f5e4f5e39c
Maksim <154775942+MIrrox27@users.noreply.github.com>
2026-06-23 17:52:23 +0500

    добавлено получение  адреса прокси

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,25 @@
 //use axum::{Router, routing::get, extract::Path};
 //use tower_http::services::ServeDir;
 use tokio::{net::TcpListener, net::TcpStream, io::AsyncBufRead, spawn};
+use std::io;
 
 
 
 
 #[tokio::main]
 async fn main() {
+    println!("Please enter proxy addres [localhost:8000]"); // Просим ввести адресс для прокси
+    let mut addr = String::new();
+    io::stdin()
+        .read_line(&mut addr)
+        .expect("Error, can't read your addres");
 
+    if addr.is_empty(){addr = "localhost:8000".to_string()}
+
+    let listner = TcpListener::bind(addr);
+    loop {
+        
+
+    }
     
 }
