56aa40f367b9ddaeffa68dee01cda9fc5ee2ed3d
Maksim <154775942+MIrrox27@users.noreply.github.com>
2026-06-23 17:43:54 +0500

    изменена структура программы

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 // author https://github.com/MIrrox27/rkn-simulator
 // src/main.rs
 
-use axum::{Router, routing::get, extract::Path};
-use tower_http::services::ServeDir;
+//use axum::{Router, routing::get, extract::Path};
+//use tower_http::services::ServeDir;
+use tokio::{net::TcpListener, net::TcpStream, io::AsyncBufRead, spawn};
 
 
 
@@ -10,32 +11,5 @@ use tower_http::services::ServeDir;
 #[tokio::main]
 async fn main() {
 
-    let app = Router::new()
-        .route("/api/*path", get(proxy_handler))
-        .nest_service("/", ServeDir::new("./static"));
     
-    let port = "localhost:8000";
-    let listner = tokio::net::TcpListener::bind(port).await.unwrap();
-
-    axum::serve(listner, app).await.unwrap();
 }
-
-
-async fn proxy_handler(Path(path): Path<String>)
- -> impl axum::response::IntoResponse {
-        // Строю URL
-    let target_url = format!("https://jsonplaceholder.typicode.com/{}", path);
-
-        // Отправляю запрос
-    let response = reqwest::get(&target_url).await.unwrap();    
-
-        // Читаю тело
-    let status = response.status();
-    //let headers = response.headers().clone();
-    let body = response.text().await.unwrap();
-
-        // возврат ответа
-    return (status, body);
-
-}
-        
